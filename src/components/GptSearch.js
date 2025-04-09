7c90f1e110e806d349f22cac3cc945218ec0de23
Imadabattuni Kumar <Imadabattuni.Kumar@smartenergywater.in>
2025-04-09 11:47:40 +0530

    bg img updated for gpt search

--- src/components/GptSearch.js
+++ src/components/GptSearch.js
@@ -1,11 +1,15 @@
 import React from 'react'
+import { netflixLogo } from '../Configuration/Constants'
 
 const GptSearch = () => {
   return (
-    <div>
-        <div className='flex'>
-            <input className='mt-48 w-5/6 my-5 border-gray-700 bg-gray-500 rounded-md py-1 px-1 text-black z-10 text-left ml-24 mr-6' type='text' placeholder='search for Movie or TV show' />
-            <button className='bg-red-600 mt-48 px-2 my-5 py-1 h-12 rounded-md w-28 text-white'>Search</button>
+    <div className='relative w-full h-screen'>
+        <div className='absolute inset-0'>
+            <img className='w-full h-full object-cover' src={netflixLogo}/>
+        </div>
+        <div className='relative flex p-4 justify-center items-start h-full'>
+            <input className='w-4/6 my-5  bg-white rounded-md py-1 px-1 text-black z-10 text-left ml-24 mr-6 h-14 mt-40' type='text' placeholder='search for Movie or TV show' />
+            <button className='bg-red-600 px-2 my-5 py-1 h-14 rounded-md w-28 text-white mt-40'>Search</button>
         </div>
     </div>
   )
